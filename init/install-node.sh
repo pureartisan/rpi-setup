5470817f01493e3c2c3376216b699da5d39dd30a
Prageeth Silva <prageethsilva@gmail.com>
2018-11-30 00:47:19 +0000

    install node cleaned up

--- init/install-node.sh
+++ init/install-node.sh
@@ -1,9 +1,25 @@
 #!/bin/bash
 
+DOWNLOAD_TAR_XZ_FILE_URL=https://nodejs.org/dist/latest-v10.x/node-v10.14.1-linux-armv6l.tar.xz
+
 cd ~
-wget -O nodejs.tar.xz https://nodejs.org/dist/latest-v10.x/node-v10.14.1-linux-armv6l.tar.xz
-tar -xvf nodejs.tar.xz
-cd nodejs
+
+# download from the specified URL
+wget -O nodejs.tar.xz $DOWNLOAD_TAR_XZ_FILE_URL
+
+# extract
+tar -xvf nodejs.tar.xz --one-top-level=nodejs-temp-download
+
+# go into the extracted directory, note there's a top level direcotry with original download file name
+re='node-.+'
+for dir in ./nodejs-temp-download/*; do
+  [[ $dir =~ $re ]] && { cd "$dir" && break; }
+done
+
+# copy to apps
 sudo cp -R * /usr/local/
-cd ..
-rm -rf nodejs
+
+# clean up
+cd ~
+rm -rf nodejs-temp-download
+rm -f nodejs.tar.xz
