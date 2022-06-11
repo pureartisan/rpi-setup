6956028b6f1bf1559249d62b1c2ee7f8d0866727
Prageeth Silva <prageeth@users.noreply.github.com>
2022-06-12 00:59:11 +1000

    Comment out apache and openhab

--- init/startup/scripts/custom-startup-scripts.sh
+++ init/startup/scripts/custom-startup-scripts.sh
@@ -8,16 +8,16 @@ echo "=== Custom Scripts: RUNNING ====================="
 touch /var/log/auth.log
 
 # apache
-sudo mkdir -p /var/log/apache2/
-sudo touch /var/log/apache2/access.log
-sudo touch /var/log/apache2/error.log
-sudo touch /var/log/apache2/other_vhosts_access.log
-sudo chown -R www-data:www-data /var/log/apache2/*
+#sudo mkdir -p /var/log/apache2/
+#sudo touch /var/log/apache2/access.log
+#sudo touch /var/log/apache2/error.log
+#sudo touch /var/log/apache2/other_vhosts_access.log
+#sudo chown -R www-data:www-data /var/log/apache2/*
 
 # openhab
-sudo mkdir -p /var/log/openhab2
-sudo touch /var/log/openhab2/events.log
-sudo touch /var/log/openhab2/openhab.log
-sudo chown -R openhab:openhab /var/log/openhab2/*
+#sudo mkdir -p /var/log/openhab2
+#sudo touch /var/log/openhab2/events.log
+#sudo touch /var/log/openhab2/openhab.log
+#sudo chown -R openhab:openhab /var/log/openhab2/*
 
 echo "=== Custom Scripts: DONE! ======================="
